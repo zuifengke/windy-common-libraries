7f675abe2b0c6147d71b4a0e5e8bee47875772ef
zuifengke <yehui1989@qq.com>
2015-08-01 09:47:12 +0800

    windy公共类库修改为maven项目

---
+++ com.windy.common.libraries/src/main/java/com/windy/com/windy/common/libraries/App.java
@@ -0,0 +1,13 @@
+package com.windy.com.windy.common.libraries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
